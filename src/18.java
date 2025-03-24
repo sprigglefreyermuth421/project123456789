445629e16538b624bcd73086ce272cbbd5e1a03f
sprigglefreyermuth421 <sprigglefreyermuth421@gmail.com>
2025-03-24 04:47:29 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,6 @@
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Code to be executed in the main method
+        System.out.println("This is a test program.");
+    }
+}
